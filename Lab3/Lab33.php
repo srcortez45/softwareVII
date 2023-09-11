2d695921cbc9e0ee1e2bce70e53da024fe6e0e42
Josseph Cortez <jcortezmg@gmail.com>
2023-09-11 08:05:57 -0500

    cambiar nombre lab 3.3

--- Lab3/Lab33.php
+++ Lab3/Lab33.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laboratorio 3.3</title>
 </head>
 
 <body>
